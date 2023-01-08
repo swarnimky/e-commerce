50f83e1ea57edf10f2e4d96becde9f4ca570b9d7
swarnimyadav <swarnim.yadav@rksv.in>
2023-01-08 22:13:14 +0530

    Added password security for clg web

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card } from "react-bootstrap";
 
 const BottomBar = () => {
+  const [password, setPassword] = useState("");
+
+  const userPrompt = () => {
+    let pass = prompt("Please enter your code");
+    setPassword(pass);
+  };
+  useEffect(() => {
+    if (password !== "cool" && password.length > 0)
+      alert("Ah! Incorrect code , Please retry");
+  }, [password]);
+
   return (
     <Card style={{ textAlign: "center" }}>
-      <a href="https://ietenotes.wapka.site/" target="_blank" rel="noreferrer">
-        On clicking here You'll enter a website based out of HTML & CSS
-      </a>
+      {password === "cool" ? (
+        <a
+          href="https://ietenotes.wapka.site/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Voila! on clicking here you'll enter a website based out of HTML & CSS{" "}
+        </a>
+      ) : (
+        <p style={{ cursor: "pointer" }} onClick={() => userPrompt()}>
+          Click here to enter code to get a link here for viewing College
+          website build by Sky's
+        </p>
+      )}
     </Card>
   );
 };
